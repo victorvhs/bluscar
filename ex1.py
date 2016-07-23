e9825c3c406dae8b7e76f64f73cb9be343015819
victorvhs <victor.h.s.reis@gmail.com>
2016-07-23 20:00:08 -0300

    Atividade 1

--- ex1.py
+++ ex1.py
@@ -1,2 +1,14 @@
+# -*- coding: utf-8 -*- 
 #Exercio numero 1
+#Faça um programa que peça dois números
+#inteiros e imprima a soma desses dois números
 
+print ("Coletânea de atividades do curso Python para zumbis")
+print("=========================")
+
+n1 = int (input ("Insira um número : "))
+n2 = int (input ("Insira o segundo número : "))
+
+soma = n1+ n2
+
+print ("%d + %d = %d" %(n1,n2,soma))
