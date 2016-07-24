7db3ea4d2c786f4a23156b85fcc4d18d29353b42
victorvhs <victor.h.s.reis@gmail.com>
2016-07-24 11:12:34 -0300

    Atividade 11

---
+++ lista_1/ex11.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*- 
+#Exercio numero 11
+#Sabendo que str( ) converte valores numéricos para string,
+#calcule quantos dígitos há em 2 elevado a um milhão.
+
+print ("Coletânea de atividades do curso Python para zumbis")
+print("=========================")
+
+milhao = str(2 **1000000)
+print("2^1000000 tem %d caracteres" %(len(milhao)))
