0dc93605d2316433f346b25f8a35ca055baab13d
victorvhs <victor.h.s.reis@gmail.com>
2016-08-15 18:20:35 -0300

    Cotação do café, 001

---
+++ Cotação Café/cotacao.py
@@ -0,0 +1,12 @@
+import urllib.request
+"""
+    Programa simples para pegar a cotação do cafe e a data atual.
+"""
+pg = urllib.request.urlopen('http://beans.itcarlow.ie/prices.html')
+texto = pg.read().decode('utf8')
+
+preco = texto[234:238]
+data = texto[290:309]
+cotacao = {data:preco}
+
+print(cotacao)
